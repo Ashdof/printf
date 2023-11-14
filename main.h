b154692a2b51cae16e1403a70702254c68361da7
Asaan12 <mathewasaan@gmail.com>
2023-11-14 13:31:34 -0800

    main file

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+struct specs
+{
+	char *specs;
+	int (*action)(va_list vl);
+};
+typedef struct specs spec_t;
+
+int _script(char);
+int scriptChar(va_list);
+int scriptString(va_list);
+int scriptPercent(va_list);
+int dissect(va_list, const char *, spec_t *);
+int _printf(const char *, ...);
+
+#endif /* MAIN_H */
